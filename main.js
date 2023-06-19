36335675cf32e4808e575d4f6b92dad411dac364
Lukasz710 <lubaszewskilukasz0@gmail.com>
2023-06-19 13:40:22 +0200

    added score incrementation animation function

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const componentsArray = document.querySelectorAll(".summary-component");
 
-async function loadData(){
+let counter = 0;
+const mainScore = 76;
+
+async function loadJSONData(){
     const response = await fetch('./data.json');
     const names = await response.json();
 
@@ -14,5 +17,20 @@ async function loadData(){
     }
 }
 
+//this function calls itself with an offset proportional to the ratio between the counter that is written to the element and it's objective
+//in this instance the objective is 76 as stated in the mainScore global variable
+//I guess if this was an actual website the score after the quiz or whatever would be stored in some variable so I didn't put it into HTML element
+function scoreIncrementation(){
+    let incrementationSpeed = counter / mainScore;
 
-loadData();
+    document.querySelector(".result-circle>h2").innerHTML = counter;
+    counter++;
+
+    if(counter <= mainScore)
+        setTimeout("scoreIncrementation()", incrementationSpeed*65);
+}
+
+window.onload = function main(){
+    scoreIncrementation();
+    loadJSONData();
+}
